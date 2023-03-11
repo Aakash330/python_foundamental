b92529a633a71afe78501d94de3ebca9ca8474cd
aakashkumar <Akashcoding@icloud.com>
2023-03-12 02:07:14 +0530

    ArithmeticOperation.py added

---
+++ ArithmeticOperation.py
@@ -0,0 +1,31 @@
+"""
+1 ->Addition        -> +
+2 -> subtraction    -> -
+3 -> Multiplication  -> *
+4 -> Division operator -> /
+5 -> Modulo operator -> %
+6 -> floor Division ->//
+7 -> Exponent  Operator or power -> **
+"""
+
+a = 9
+b = 2
+print("Addition = ", a+b)
+print("subtract = ", a-b)
+print("multiplication = ", a*b)
+print("division =", a/b)
+print("Modulo operator ", a % b)
+print("floor Division ", a // b)
+print("Exponent  Operator or power ", a ** b)
+
+# output
+"""
+Addition =  11
+subtract =  7
+multiplication =  18
+division = 4.5
+Modulo operator  1
+floor Division  4
+Exponent  Operator or power  81
+"""
+
