310b217ac8963750d40715a9467d0dc52b12b560
aakashkumar <Akashcoding@icloud.com>
2023-03-13 02:43:15 +0530

    math module example

---
+++ MathModule.py
@@ -0,0 +1,21 @@
+from math import pi
+from math import ceil
+from math import floor
+from math import gcd
+
+r = float(input("Enter the radius of circle :"))
+print("Area of cirecle is :", 2 * pi * r)
+
+"""
+There are many function pre build in math module
+Like :-
+ceil(x)
+floor(x)
+etc
+"""
+
+num = float(input("Enter any float number : "))
+print("ceil value of {} number is {} ".format(num, ceil(num)))
+print("floor value of {} number is {} ".format(num, floor(num)))
+print("floor value of {} number is {} ".format(num, floor(num)))
+
